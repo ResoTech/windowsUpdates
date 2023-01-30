fd3cf1748298d11ec19af99574e6ca55223a60f2
Christopher Kelley <christopher@cktech.org>
2023-01-30 13:42:35 -0500

    Update updateWindows.ps1
    Add registry value change

--- updateWindows.ps1
+++ updateWindows.ps1
@@ -1,6 +1,12 @@
 # Set execution policy to unrestricted
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 
+#registry tweaks 
+$registryPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell"
+$executionTimeout = 10800000
+
+Set-ItemProperty -Path $registryPath -Name "ExecutionTimeout" -Value $executionTimeout -Force
+
 # Install the PSWindowsUpdate module
 Install-Module -Name PSWindowsUpdate
 
