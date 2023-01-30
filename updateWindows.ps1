70059758869fd02390f8fd511c59d9a5e19a48b5
Christopher Kelley <christopher@cktech.org>
2023-01-30 13:24:54 -0500

    Update updateWindows.ps1
    timeout /t 10800

--- updateWindows.ps1
+++ updateWindows.ps1
@@ -14,7 +14,9 @@ $Updates = Get-WUInstall
 if ($Updates) {
     Write-Output "Installing updates..."
     Install-WindowsUpdate -Install -AcceptAll -AutoReboot
+    timeout /t 10800
 }
 else {
     Write-Output "No updates available."
+    
 }
